1306eea6f1ebcce577620a75ead36a8b4fd4c09d
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2025-01-20 12:26:33 -0800

    setup up the api module

--- app/src/main/java/com/nolawiworkineh/comments/data/di/HayleyApiModule.kt
+++ app/src/main/java/com/nolawiworkineh/comments/data/di/HayleyApiModule.kt
@@ -1,12 +1,27 @@
 package com.nolawiworkineh.comments.data.di
 
+import com.nolawiworkineh.comments.data.network.HayleyApiService
+import com.nolawiworkineh.comments.data.network.HayleyRetrofitClient
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
 
 
 @Module
 @InstallIn(SingletonComponent::class)
 object HayleyApiModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit() : Retrofit = HayleyRetrofitClient.retrofit
+
+    @Provides
+    @Singleton
+    fun provideHayleyApiService(retrofit: Retrofit): HayleyApiService =
+        retrofit.create(HayleyApiService::class.java)
+
 
 }
